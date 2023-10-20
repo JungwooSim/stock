dc8737f1b2b1af9a21484703806fb56aa9a48ddc
JungwooSim <cscd053@gmail.com>
2023-10-20 23:40:27 +0900

    add : entity 수정

--- infrastructure/src/main/kotlin/com/stock/infrastructure/db/TransactionEntity.kt
+++ infrastructure/src/main/kotlin/com/stock/infrastructure/db/TransactionEntity.kt
@@ -2,12 +2,14 @@ package com.stock.infrastructure.db
 
 import com.stock.common.enums.TransactionKindEnum
 import com.stock.common.enums.TransactionStateEnum
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import java.math.BigDecimal
 import java.time.LocalDateTime
 
+// TODO : 환경별 DB 설정 파일 추가하기
 @Table("transaction")
-class TransactionEntity(
+class TransactionEntity constructor(
   val id: Long? = null,
   val kind: TransactionKindEnum,
   val state: TransactionStateEnum,
@@ -16,6 +18,10 @@ class TransactionEntity(
   val userId: Long,
   val stockId: Long,
   val orderNo: Long,
+
+  @Column("modified_at")
   val modifiedAt: LocalDateTime,
+
+  @Column("created_at")
   val createdAt: LocalDateTime
 )
