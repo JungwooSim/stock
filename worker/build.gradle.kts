d5bd0ee86b63538f8f92a5a89741186a6f152214
JungwooSim <cscd053@gmail.com>
2024-01-01 00:58:24 +0900

    add : worker - local yml

--- worker/build.gradle.kts
+++ worker/build.gradle.kts
@@ -1,11 +1,10 @@
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.kafka:spring-kafka")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
 
   implementation(project(":common"))
-//  implementation(project(":infrastructure"))
+  implementation(project(":infrastructure"))
 }
 
 tasks.withType<Test> {
