c887932433ac2da80e404a8524977348522821a9
JungwooSim <cscd053@gmail.com>
2024-01-03 22:02:17 +0900

    add : thymeleaf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,9 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+  implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.0")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.projectreactor:reactor-test")
 
--- app/src/main/kotlin/com/stock/interfaces/ViewController.kt
+++ app/src/main/kotlin/com/stock/interfaces/ViewController.kt
@@ -1,4 +1,14 @@
 package com.stock.interfaces
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class ViewController {
+
+  @GetMapping("/view/stock")
+  fun stock(): String {
+    return "html/index"
+  }
+  
 }
